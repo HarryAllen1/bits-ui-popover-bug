a65ef66d3d07d79e250276b1fb34057247fd335f
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2024-10-18 13:09:17 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Popover } from 'bits-ui';
+</script>
+
+<Popover.Root>
+  <Popover.Trigger>popover trigger</Popover.Trigger>
+  <Popover.Content>asdfafsdjsdfkaljl</Popover.Content>
+</Popover.Root>
